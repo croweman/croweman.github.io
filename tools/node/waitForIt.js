c1dd82e534c6cc9875a373c87bae2ab3984732ae
Lee Crowe <790926+croweman@users.noreply.github.com>
2023-03-14 16:14:01 +0000

    Create waitForIt.js

---
+++ tools/node/waitForIt.js
@@ -0,0 +1,20 @@
+const waitForAssertions = (asyncAssertionFunc, timeoutMilliseconds = 2000, failureMessage) => new Promise((resolve, reject) => {
+    let timeoutId
+
+    const intervalId = setInterval(async () => {
+        try {
+            await asyncAssertionFunc()
+            clearTimeout(timeoutId)
+            clearInterval(intervalId)
+            return resolve();
+        } catch (err) {
+            // do nothing
+        }
+    }, 10)
+
+    timeoutId = setTimeout(() => {
+        clearInterval(intervalId)
+        let message = failureMessage ?? `Assertion conditions were not met within ${timeoutMilliseconds} milliseconds`
+        reject(new Error(message))
+    }, timeoutMilliseconds)
+})
