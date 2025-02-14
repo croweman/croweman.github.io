c315881297b5aaf3bbedd545fa30e62f84365e6e
Lee Crowe <lee.crowe@comparethemarket.com>
2025-02-14 10:29:10 +0000

    .

--- croweman-secure-data/lib/encryptors/symmetric.js
+++ croweman-secure-data/lib/encryptors/symmetric.js
@@ -1,27 +1,3 @@
-// 'use strict'
-//
-// const crypto = require('crypto'),
-//     algorithm = 'aes-256-ctr';
-//
-// function encrypt(password, text){
-//
-//     var cipher = crypto.createCipher(algorithm, password)
-//     var crypted = cipher.update(text, 'utf8', 'hex')
-//     return crypted + cipher.final('hex');
-// }
-//
-// function decrypt(password, text){
-//
-//     var decipher = crypto.createDecipher(algorithm, password)
-//     var dec = decipher.update(text, 'hex', 'utf8')
-//     return dec + decipher.final('utf8');
-// }
-//
-// module.exports = {
-//     encrypt,
-//     decrypt
-// }
-
 'use strict'
 
 const crypto = require('crypto'),
